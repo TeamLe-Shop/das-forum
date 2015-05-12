7162c7ba7f05407972b96574e49c011de87facc1
Delu Anachebe <cjaf.javacakess@gmail.com>
2015-05-12 20:01:12 +0100

    Use standard functions (strncat)

--- src/util.c
+++ src/util.c
@@ -25,14 +25,14 @@ void address_to_string(IPAddress address, char* string, bool include_port)
         inet_ntop(AF_INET, &ip4addr.sin_addr, string, INET_ADDRSTRLEN);
         if (include_port) {
             sprintf(port_str, ":%d", ip4addr.sin_port);
-            strlcat(string, port_str, IP_MAXSTRLEN);
+            strncat(string, port_str, IP_MAXSTRLEN);
         }
     } else if (address.ss_family == AF_INET6) {
         IP6Address ip6addr = *(IP6Address*)&address;
         inet_ntop(AF_INET6, &ip6addr.sin6_addr, string, INET6_ADDRSTRLEN);
         if (include_port) {
             sprintf(port_str, ":%d", ip6addr.sin6_port);
-            strlcat(string, port_str, IP_MAXSTRLEN);
+            strncat(string, port_str, IP_MAXSTRLEN);
         }
     }
 }
